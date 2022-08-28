ab06f2f96241ecbd95f55a64bc8c88f4e21664c7
Rashid Alkhoev <mr.alhoev@mail.ru>
2022-08-28 16:09:06 +0300

    done with subgraph

--- generated/schema.ts
+++ generated/schema.ts
@@ -11,7 +11,7 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
-export class ExampleEntity extends Entity {
+export class Event extends Entity {
   constructor(id: string) {
     super();
     this.set("id", Value.fromString(id));
@@ -19,18 +19,18 @@ export class ExampleEntity extends Entity {
 
   save(): void {
     let id = this.get("id");
-    assert(id != null, "Cannot save ExampleEntity entity without an ID");
+    assert(id != null, "Cannot save Event entity without an ID");
     if (id) {
       assert(
         id.kind == ValueKind.STRING,
-        `Entities of type ExampleEntity must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+        `Entities of type Event must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
       );
-      store.set("ExampleEntity", id.toString(), this);
+      store.set("Event", id.toString(), this);
     }
   }
 
-  static load(id: string): ExampleEntity | null {
-    return changetype<ExampleEntity | null>(store.get("ExampleEntity", id));
+  static load(id: string): Event | null {
+    return changetype<Event | null>(store.get("Event", id));
   }
 
   get id(): string {
@@ -42,15 +42,6 @@ export class ExampleEntity extends Entity {
     this.set("id", Value.fromString(value));
   }
 
-  get count(): BigInt {
-    let value = this.get("count");
-    return value!.toBigInt();
-  }
-
-  set count(value: BigInt) {
-    this.set("count", Value.fromBigInt(value));
-  }
-
   get eventID(): Bytes {
     let value = this.get("eventID");
     return value!.toBytes();
@@ -60,12 +51,355 @@ export class ExampleEntity extends Entity {
     this.set("eventID", Value.fromBytes(value));
   }
 
-  get attendeeAddress(): Bytes {
-    let value = this.get("attendeeAddress");
+  get name(): string | null {
+    let value = this.get("name");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set name(value: string | null) {
+    if (!value) {
+      this.unset("name");
+    } else {
+      this.set("name", Value.fromString(<string>value));
+    }
+  }
+
+  get description(): string | null {
+    let value = this.get("description");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set description(value: string | null) {
+    if (!value) {
+      this.unset("description");
+    } else {
+      this.set("description", Value.fromString(<string>value));
+    }
+  }
+
+  get link(): string | null {
+    let value = this.get("link");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set link(value: string | null) {
+    if (!value) {
+      this.unset("link");
+    } else {
+      this.set("link", Value.fromString(<string>value));
+    }
+  }
+
+  get imageURL(): string | null {
+    let value = this.get("imageURL");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set imageURL(value: string | null) {
+    if (!value) {
+      this.unset("imageURL");
+    } else {
+      this.set("imageURL", Value.fromString(<string>value));
+    }
+  }
+
+  get eventOwner(): Bytes {
+    let value = this.get("eventOwner");
     return value!.toBytes();
   }
 
-  set attendeeAddress(value: Bytes) {
-    this.set("attendeeAddress", Value.fromBytes(value));
+  set eventOwner(value: Bytes) {
+    this.set("eventOwner", Value.fromBytes(value));
+  }
+
+  get eventTimestamp(): BigInt {
+    let value = this.get("eventTimestamp");
+    return value!.toBigInt();
+  }
+
+  set eventTimestamp(value: BigInt) {
+    this.set("eventTimestamp", Value.fromBigInt(value));
+  }
+
+  get maxCapacity(): BigInt {
+    let value = this.get("maxCapacity");
+    return value!.toBigInt();
+  }
+
+  set maxCapacity(value: BigInt) {
+    this.set("maxCapacity", Value.fromBigInt(value));
+  }
+
+  get deposit(): BigInt {
+    let value = this.get("deposit");
+    return value!.toBigInt();
+  }
+
+  set deposit(value: BigInt) {
+    this.set("deposit", Value.fromBigInt(value));
+  }
+
+  get paidOut(): boolean {
+    let value = this.get("paidOut");
+    return value!.toBoolean();
+  }
+
+  set paidOut(value: boolean) {
+    this.set("paidOut", Value.fromBoolean(value));
+  }
+
+  get totalRSVPs(): BigInt {
+    let value = this.get("totalRSVPs");
+    return value!.toBigInt();
+  }
+
+  set totalRSVPs(value: BigInt) {
+    this.set("totalRSVPs", Value.fromBigInt(value));
+  }
+
+  get totalConfirmedAttendees(): BigInt {
+    let value = this.get("totalConfirmedAttendees");
+    return value!.toBigInt();
+  }
+
+  set totalConfirmedAttendees(value: BigInt) {
+    this.set("totalConfirmedAttendees", Value.fromBigInt(value));
+  }
+
+  get rsvps(): Array<string> | null {
+    let value = this.get("rsvps");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toStringArray();
+    }
+  }
+
+  set rsvps(value: Array<string> | null) {
+    if (!value) {
+      this.unset("rsvps");
+    } else {
+      this.set("rsvps", Value.fromStringArray(<Array<string>>value));
+    }
+  }
+
+  get confirmedAttendees(): Array<string> | null {
+    let value = this.get("confirmedAttendees");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toStringArray();
+    }
+  }
+
+  set confirmedAttendees(value: Array<string> | null) {
+    if (!value) {
+      this.unset("confirmedAttendees");
+    } else {
+      this.set(
+        "confirmedAttendees",
+        Value.fromStringArray(<Array<string>>value)
+      );
+    }
+  }
+}
+
+export class Account extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save Account entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type Account must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("Account", id.toString(), this);
+    }
+  }
+
+  static load(id: string): Account | null {
+    return changetype<Account | null>(store.get("Account", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get totalRSVPs(): BigInt {
+    let value = this.get("totalRSVPs");
+    return value!.toBigInt();
+  }
+
+  set totalRSVPs(value: BigInt) {
+    this.set("totalRSVPs", Value.fromBigInt(value));
+  }
+
+  get totalAttendedEvents(): BigInt {
+    let value = this.get("totalAttendedEvents");
+    return value!.toBigInt();
+  }
+
+  set totalAttendedEvents(value: BigInt) {
+    this.set("totalAttendedEvents", Value.fromBigInt(value));
+  }
+
+  get rsvps(): Array<string> | null {
+    let value = this.get("rsvps");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toStringArray();
+    }
+  }
+
+  set rsvps(value: Array<string> | null) {
+    if (!value) {
+      this.unset("rsvps");
+    } else {
+      this.set("rsvps", Value.fromStringArray(<Array<string>>value));
+    }
+  }
+
+  get attendedEvents(): Array<string> | null {
+    let value = this.get("attendedEvents");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toStringArray();
+    }
+  }
+
+  set attendedEvents(value: Array<string> | null) {
+    if (!value) {
+      this.unset("attendedEvents");
+    } else {
+      this.set("attendedEvents", Value.fromStringArray(<Array<string>>value));
+    }
+  }
+}
+
+export class RSVP extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save RSVP entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type RSVP must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("RSVP", id.toString(), this);
+    }
+  }
+
+  static load(id: string): RSVP | null {
+    return changetype<RSVP | null>(store.get("RSVP", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get attendee(): string {
+    let value = this.get("attendee");
+    return value!.toString();
+  }
+
+  set attendee(value: string) {
+    this.set("attendee", Value.fromString(value));
+  }
+
+  get event(): string {
+    let value = this.get("event");
+    return value!.toString();
+  }
+
+  set event(value: string) {
+    this.set("event", Value.fromString(value));
+  }
+}
+
+export class Confirmation extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save Confirmation entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type Confirmation must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("Confirmation", id.toString(), this);
+    }
+  }
+
+  static load(id: string): Confirmation | null {
+    return changetype<Confirmation | null>(store.get("Confirmation", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get attendee(): string {
+    let value = this.get("attendee");
+    return value!.toString();
+  }
+
+  set attendee(value: string) {
+    this.set("attendee", Value.fromString(value));
+  }
+
+  get event(): string {
+    let value = this.get("event");
+    return value!.toString();
+  }
+
+  set event(value: string) {
+    this.set("event", Value.fromString(value));
   }
 }
--- src/web-3-rsvp.ts
+++ src/web-3-rsvp.ts
@@ -1,59 +1,119 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, ipfs, json } from "@graphprotocol/graph-ts";
 import {
-  Web3RSVP,
   ConfirmedAttendee,
-  DepositsPaidOut,
   NewEventCreated,
-  NewRSVP
-} from "../generated/Web3RSVP/Web3RSVP"
-import { ExampleEntity } from "../generated/schema"
+  NewRSVP,
+  DepositsPaidOut,
+} from "../generated/Web3RSVP/Web3RSVP";
+import { Account, RSVP, Confirmation, Event } from "../generated/schema";
+import { integer } from "@protofire/subgraph-toolkit";
 
-export function handleConfirmedAttendee(event: ConfirmedAttendee): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleNewEventCreated(event: NewEventCreated): void {
+  let newEvent = Event.load(event.params.eventID.toHex());
+  if (newEvent == null) {
+    newEvent = new Event(event.params.eventID.toHex());
+    newEvent.eventID = event.params.eventID;
+    newEvent.eventOwner = event.params.creatorAddress;
+    newEvent.eventTimestamp = event.params.eventTimestamp;
+    newEvent.maxCapacity = event.params.maxCapacity;
+    newEvent.deposit = event.params.deposit;
+    newEvent.paidOut = false;
+    newEvent.totalRSVPs = integer.ZERO;
+    newEvent.totalConfirmedAttendees = integer.ZERO;
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    let metadata = ipfs.cat(event.params.eventDataCID + "/data.json");
+    
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    if (metadata) {
+      const value = json.fromBytes(metadata).toObject();
+      if (value) {
+        const name = value.get("name");
+        const description = value.get("description");
+        const link = value.get("link");
+        const imagePath = value.get("image");
+
+        if (name) {
+          newEvent.name = name.toString();
+        }
+
+        if (description) {
+          newEvent.description = description.toString();
+        }
+
+        if (link) {
+          newEvent.link = link.toString();
+        }
+
+        if(imagePath){
+          const imageURL =
+      "https://ipfs.io/ipfs/" + event.params.eventDataCID + imagePath.toString();
+          newEvent.imageURL = imageURL;
+        } else {
+          const fallbackURL = "https://ipfs.io/ipfs/bafybeibssbrlptcefbqfh4vpw2wlmqfj2kgxt3nil4yujxbmdznau3t5wi/event.png";
+          newEvent.imageURL = fallbackURL;
+        }
+
+        
+      }
+    }
+
+    newEvent.save();
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.eventID = event.params.eventID
-  entity.attendeeAddress = event.params.attendeeAddress
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.idToEvent(...)
-  // - contract.owner(...)
-  // - contract.unlockTime(...)
 }
 
-export function handleDepositsPaidOut(event: DepositsPaidOut): void {}
+function getOrCreateAccount(address: Address): Account {
+  let account = Account.load(address.toHex());
+  if (account == null) {
+    account = new Account(address.toHex());
+    account.totalRSVPs = integer.ZERO;
+    account.totalAttendedEvents = integer.ZERO;
+    account.save();
+  }
+  return account;
+}
 
-export function handleNewEventCreated(event: NewEventCreated): void {}
+export function handleNewRSVP(event: NewRSVP): void {
+  let id = event.params.eventID.toHex() + event.params.attendeeAddress.toHex();
+  let newRSVP = RSVP.load(id);
+  let account = getOrCreateAccount(event.params.attendeeAddress);
+  let thisEvent = Event.load(event.params.eventID.toHex());
+  if (newRSVP == null && thisEvent != null) {
+    newRSVP = new RSVP(id);
+    newRSVP.attendee = account.id;
+    newRSVP.event = thisEvent.id;
+    newRSVP.save();
+    thisEvent.totalRSVPs = integer.increment(thisEvent.totalRSVPs);
+    thisEvent.save();
+    account.totalRSVPs = integer.increment(account.totalRSVPs);
+    account.save();
+  }
+}
 
-export function handleNewRSVP(event: NewRSVP): void {}
+export function handleConfirmedAttendee(event: ConfirmedAttendee): void {
+  let id = event.params.eventID.toHex() + event.params.attendeeAddress.toHex();
+  let newConfirmation = Confirmation.load(id);
+  let account = getOrCreateAccount(event.params.attendeeAddress);
+  let thisEvent = Event.load(event.params.eventID.toHex());
+  if (newConfirmation == null && thisEvent != null) {
+    newConfirmation = new Confirmation(id);
+    newConfirmation.attendee = account.id;
+    newConfirmation.event = thisEvent.id;
+    newConfirmation.save();
+    thisEvent.totalConfirmedAttendees = integer.increment(
+      thisEvent.totalConfirmedAttendees
+    );
+    thisEvent.save();
+    account.totalAttendedEvents = integer.increment(
+      account.totalAttendedEvents
+    );
+    account.save();
+  }
+}
+
+export function handleDepositsPaidOut(event: DepositsPaidOut): void {
+  let thisEvent = Event.load(event.params.eventID.toHex());
+  if (thisEvent) {
+    thisEvent.paidOut = true;
+    thisEvent.save();
+  }
+}
